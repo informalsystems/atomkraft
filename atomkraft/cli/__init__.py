8a2ba04751f695a7da0bf45f7d3803babe8980a0
ivan-gavran <ivan@informal.systems>
2022-07-20 10:21:30 +0200

    reading command line options

--- atomkraft/cli/__init__.py
+++ atomkraft/cli/__init__.py
@@ -2,9 +2,11 @@ from typing import List, Optional
 
 import pytest
 import typer
+import os
 from copier import run_auto
 
 from .. import chain
+from atomkraft import Reactor
 
 app = typer.Typer(name="atomkraft", no_args_is_help=True)
 
@@ -23,6 +25,25 @@ def smoke_test():
     pytest.main([".atomkraft/smoke_tests"])
 
 
+# run with:
+# poetry run atomkraft reactor --actions-list=act1,act2,act3 --variables-list=x,y,z --reactor-stub-file=path/to/the/file
+# or
+# poetry run atomkraft reactor --actions-list="act1, act2, act3" --variables-list="x, y, z" --reactor-stub-file="path/to/the/file"
+
+
 @app.command()
-def reactor():
-    print("Generating a reactor...")
+def reactor(
+    variables_list: Optional[str] = typer.Option(
+        None, help="A list of state variables to include into reactors as parameters"
+    ),
+    actions_list: Optional[str] = typer.Option(
+        None, help="A list of actions for which to create reactor stubs."
+    ),
+    reactor_stub_file: str = typer.Option(
+        os.path.abspath("atomkraft/reactors/reactor.py"),
+        help="A path where to create the reactors file.",
+    ),
+):
+    actions = [act.strip() for act in actions_list.split(",")]
+    variables = [var.strip() for var in variables_list.split(",")]
+    Reactor.generate_reactor(actions, variables, reactor_stub_file)
