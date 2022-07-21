8c18f34e275116d55550d393adcdd79560413370
Ranadeep Biswas <mail@rnbguy.at>
2022-07-21 14:20:17 +0200

    remove local template

--- atomkraft/cli/__init__.py
+++ atomkraft/cli/__init__.py
@@ -1,27 +1,20 @@
-import os
 from pathlib import Path
-from typing import List, Optional
 
+import git
 import pytest
 import typer
 from copier import run_auto
-from git import Repo
 
 from .. import chain
 
 app = typer.Typer(name="atomkraft", no_args_is_help=True)
 
-cwd = os.getcwd()
-git_repo = Repo(cwd, search_parent_directories=True)
-git_root = git_repo.git.rev_parse("--show-toplevel")
-
 GH_TEMPLATE = "gh:informalsystems/atomkraft"
-LOCAL_TEMPLATE = f"{git_root}"
 
 
 @app.command()
 def init(path: Path):
-    Repo.init(path)
+    git.Repo.init(path)
     run_auto(GH_TEMPLATE, path, vcs_ref="rano/impl-adr02")
 
 
