e9c717f02470c856fcddd23a79fd145d5e3e3c0b
hvanz <hernan.vanzetto@gmail.com>
2022-07-28 17:12:42 +0300

    Add config classes

---
+++ atomkraft/config/config_file.py
@@ -0,0 +1,28 @@
+import tomlkit
+
+
+class ConfigFile:
+    def __init__(self, path: str):
+        self.data = {}
+        self.path = path
+
+    def __enter__(self):
+        self.fd = open(self.path, 'w+')
+        self.data = tomlkit.load(self.fg)
+        return self
+    
+    def __exit__(self, type, value, traceback):
+        self.fd.close()
+    
+    def get(self, key) -> str:
+        if key in self.data:
+            return self.data[key]        
+        if key.replace("-", "_") in self.data:
+            return self.data[key.replace("-", "_")]
+        if key.replace("_", "-") in self.data:
+            return self.data[key.replace("_", "-")]
+        raise KeyError
+
+    def write(self, key, value):
+        self.data[key] = value
+        tomlkit.dump(self.data, self.fd)
---
+++ atomkraft/config/model_config.py
@@ -0,0 +1,17 @@
+import os
+from typing import Optional
+from atomkraft.config.config_file import ConfigFile
+from atomkraft.utils.project import project_root
+
+MODEL_CONFIG_PATH = "model.toml"
+
+class ModelConfig(ConfigFile):
+
+    def __init__(self, path: Optional[str] = None):
+        if not path:
+            if "PYTEST_CURRENT_TEST" in os.environ:
+                root = "tests/project"
+            else:
+                root = project_root()
+            self.path = os.path.join(root, MODEL_CONFIG_PATH)
+        super(path)
