634ef318407320aa4c7733d95b43e4a83f9f207e
Ranadeep Biswas <mail@rnbguy.at>
2022-08-12 16:55:02 +0200

    update template

--- atomkraft/utils/project.py
+++ atomkraft/utils/project.py
@@ -5,8 +5,10 @@ from pathlib import Path
 def project_root():
     cwd = Path(os.getcwd())
     while cwd != cwd.parent:
-        if (cwd / "atomkraft.toml").exists() and (
-            cwd / ".atomkraft" / "config.toml"
-        ).exists():
+        if (
+            (cwd / "pyproject.toml").exists()
+            and (cwd / "atomkraft.toml").exists()
+            and (cwd / ".atomkraft" / "config.toml").exists()
+        ):
             return cwd
         cwd = cwd.parent
