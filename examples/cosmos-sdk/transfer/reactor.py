f502d81091d9050d927b354f0fbbadf4975a14f4
Ranadeep Biswas <mail@rnbguy.at>
2022-09-02 14:30:25 +0200

    update reactor

--- examples/cosmos-sdk/transfer/reactor.py
+++ examples/cosmos-sdk/transfer/reactor.py
@@ -1,66 +1,39 @@
 import logging
 import time
 
+from atomkraft.chain import Testnet
 from modelator.pytest.decorators import step
-from terra_sdk.client.lcd import LCDClient
-from terra_sdk.client.lcd.api.tx import CreateTxOptions
-from terra_sdk.core import Coin, Coins
 from terra_sdk.core.bank import MsgSend
-from terra_sdk.core.fee import Fee
-from terra_sdk.key.mnemonic import MnemonicKey
 
 
 @step("Init")
-def init(testnet, action):
+def init(testnet: Testnet, action):
     logging.info("Step: Init")
-    testnet.n_account = action["value"]["n_wallet"]
+    testnet.set_accounts(action.value.wallets)
     testnet.verbose = True
-
     testnet.oneshot()
     time.sleep(10)
-
     logging.info("Status: Testnet launched\n")
 
 
 @step("Transfer")
-def transfer(testnet, action):
+def transfer(testnet: Testnet, action):
     logging.info("Step: Transfer")
+    action = action.value
 
-    rest_endpoint = testnet.get_validator_port(0, "lcd")
-    lcdclient = LCDClient(
-        url=rest_endpoint,
-        chain_id=testnet.chain_id,
-        gas_prices=f"10{testnet.denom}",
-        gas_adjustment=0.1,
-    )
+    sender_id = action.sender
+    receiver_id = action.receiver
+    amount = action.amount
 
-    sender_id = action["value"]["sender"]
-    receiver_id = action["value"]["receiver"]
-    amount = action["value"]["amount"]
+    sender_addr = testnet.acc_addr(sender_id)
+    receiver_addr = testnet.acc_addr(receiver_id)
 
-    sender = testnet.accounts[sender_id].address(testnet.prefix)
-    receiver = testnet.accounts[receiver_id].address(testnet.prefix)
+    msg = MsgSend(sender_addr, receiver_addr, f"{amount}{testnet.denom}")
 
-    sender_wallet = lcdclient.wallet(
-        MnemonicKey(
-            mnemonic=testnet.accounts[sender_id].mnemonic,
-            coin_type=testnet.coin_type,
-            prefix=testnet.prefix,
-        )
-    )
+    result = testnet.broadcast_transaction(sender_id, msg, gas=200000, fee_amount=20000)
 
-    msg = MsgSend(sender, receiver, Coins([Coin(testnet.denom, amount)]))
-
-    tx = sender_wallet.create_and_sign_tx(
-        CreateTxOptions(
-            msgs=[msg], fee=Fee(20000000, Coins([Coin(testnet.denom, 2000000)]))
-        )
-    )
-
-    result = lcdclient.tx.broadcast(tx)
-
-    logging.info(f"\tSender:    {msg.from_address}")
-    logging.info(f"\tReceiver:  {msg.to_address}")
+    logging.info(f"\tSender:    {sender_id} ({sender_addr})")
+    logging.info(f"\tReceiver:  {receiver_id} ({receiver_addr})")
     logging.info(f"\tAmount:    {msg.amount}")
 
     if result.code == 0:
@@ -73,4 +46,4 @@ def transfer(testnet, action):
     logging.debug(f"[MSG] {msg}")
     logging.debug(f"[RES] {result}")
 
-    time.sleep(2)
+    time.sleep(1)
