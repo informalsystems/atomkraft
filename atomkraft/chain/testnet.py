55093eae0d567c2fce80c718cbfa9ceb80fa3192
Rano | Ranadeep <mail@rnbguy.at>
2022-10-27 15:11:05 +0200

    Update atomkraft/chain/testnet.py

--- atomkraft/chain/testnet.py
+++ atomkraft/chain/testnet.py
@@ -385,15 +385,9 @@ class Testnet:
             service = ServiceStub(channel)
 
             with TmEventSubscribe({"tm.event": "Tx"}) as subscriber:
-                # # BROADCAST_MODE_BLOCK defines a tx broadcasting mode where the client waits
-                # # for the tx to be committed in a block.
-                # BROADCAST_MODE_BLOCK = 1
                 # # BROADCAST_MODE_SYNC defines a tx broadcasting mode where the client waits
                 # # for a CheckTx execution response only.
                 # BROADCAST_MODE_SYNC = 2
-                # # BROADCAST_MODE_ASYNC defines a tx broadcasting mode where the client
-                # # returns immediately.
-                # BROADCAST_MODE_ASYNC = 3
                 result = asyncio.run(
                     service.broadcast_tx(
                         tx_bytes=bytes(tx.to_proto()),
